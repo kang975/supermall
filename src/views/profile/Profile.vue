bff9c1d970cd931ec9a674f537a37d0fc7eebf8f
kang975 <2572667457@qq.com>
2020-02-24 15:17:19 +0800

    3

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,11 +1,11 @@
 <template>
-  <h2>个人</h2>
+    <h2>个人</h2>
 </template>
 
 <script>
-  export default {
-    name: "Profile"
-  }
+    export default {
+        name: "Profile"
+    }
 </script>
 
 <style scoped>
